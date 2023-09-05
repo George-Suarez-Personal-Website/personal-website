25216b420e78f3490e813571c9f0d1e5703433c9
George Suarez <georgejsuarez@gmail.com>
2023-09-05 14:04:59 -0700

    Created cool background using particles.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,9 @@
 console.log("Hello World!");
+
+window.onload = function() {
+    Particles.init({
+        selector: '.background',
+        maxParticles: 150,
+        connectParticles: true
+    });
+};
